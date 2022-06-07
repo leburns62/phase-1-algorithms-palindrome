3a2b7b0ca71f330b89b7e425c27cd6bea81472fd
Levi Hutchinson Burns <leburns62@gmail.com>
2022-06-06 20:28:13 -0400

    commit tree 7a1767dfef66f5abf2198d35f3cdcc3185852c67

--- index.js
+++ index.js
@@ -1,25 +1,40 @@
-function isPalindrome(word) {
-  // Write your algorithm here
-}
 
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
-
-  console.log("");
-
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
-}
-
-module.exports = isPalindrome;
+  function reverse(word) {
+    const wordArray = word.split("");
+    const reversedWordArray = wordArray.reverse();
+    const reversedWord = reversedWordArray.join("");
+    return reversedWord;
+  
+  }
+  
+  function isPalindrome(word) {
+    const reversedWord = reverse(word);
+  
+    if (word === reversedWord){
+      return true
+    } else {
+      return false
+    }
+  }
+  
+  /* 
+    Add your pseudocode here
+  /
+  
+  /
+    Add written explanation of your solution here
+  */
+  
+  // You can run node index.js to view these console logs
+  if (require.main === module) {
+    // add your own custom tests in here
+    console.log("Expecting: true");
+    console.log("=>", isPalindrome("racecar"));
+  
+    console.log("");
+  
+    console.log("Expecting: false");
+    console.log("=>", isPalindrome("robot"));
+  }
+  
+  module.exports = isPalindrome;
